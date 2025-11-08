135cc82fb4b6b6ca69e9bcdbc40e649aff334c4b
Ivanp2003 <andrespanchichavez@gmail.com>
2025-11-08 00:48:34 -0500

    Segundo avance, utilizando postman

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 
+const app = express();
+app.use(bodyParser.json());//Para convertir en .json
+app.use(bodyParser.urlencoded({extended:true}));//Empaquetamiendo de archivos
 const PORT = process.env.PORT || 3000;
 
 app.get('/',(req,res)=>{
@@ -12,7 +15,47 @@ app.get('/',(req,res)=>{
         <h2>Autor: Andrés Panchi</h2>
         <p>Video de referencia: https://platzi.com/cursos/expressjs/</p>
         `);
-})
+});
+
+app.get('/users/:id',(req,res)=>{
+    const userId = req.params.id;// Con params vamos a poder ejecutar la url en get
+    res.send(`Mostrar informacion del usuario con ID:${userId}`);// Manda una respuesta
+});
+
+app.get('search',(req,res)=>{
+    const terms = req.query.termino || 'No especifica';
+    const category = req.query.categoria || 'Todas';
+    res.send(`
+        <h2>Resultados de Búsqueda</h2>
+        <p>Término: ${terms}</p>
+        <p>Categoría: ${category}</p>
+        `);
+});
+
+
+app.post('/form',(req,res)=>{
+    const name = req.body.nombre || 'Anónimo';
+    const email = req.body.email || 'No proporcionado';
+    res.json({
+        message:'Datos recibidos',
+        data:{
+            name,
+            email
+        }
+    })
+});
+
+app.post('/api/data',(req,res)=>{
+    const data = req.body;
+    if(!data || Object.keys(data).length===0){
+        return res.status(400).json({error:'No se recibieron datos'});
+    }
+
+    res.status(201).json({
+        message:'Datos json recibidos',
+        data
+    });
+});
 
 app.listen(PORT,()=>{
     console.log(`Servidor: http://localhost:${PORT}`);
